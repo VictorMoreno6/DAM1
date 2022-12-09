85dde42774f8641f1b7488482305256b00fdf544
1dam <victor.moreno7@educa.madrid.org>
2022-12-09 11:57:10 +0100

    Prueba Git

---
+++ Strings/src/strings/Strings.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package strings;
+
+/**
+ *
+ * @author 1dam
+ */
+public class Strings {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola soy Victor Moreno");
+        System.out.println("Estamos en Netbeants");
+    }
+    
+}
